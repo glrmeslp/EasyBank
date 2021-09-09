f0721df694e57d865a4a33023924191ccfa2ffb1
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-09 14:21:05 -0400

    update qr code view model

--- EasyBank/Scenes/QRCode/QRCodeViewModel.swift
+++ EasyBank/Scenes/QRCode/QRCodeViewModel.swift
@@ -3,24 +3,25 @@ protocol QRcodeViewModelCoordinatorDelegate: AnyObject {
 }
 
 final class QRCodeViewModel: BaseViewModel {
-
-    private var roomName: String
-    private var value: Double
+    private let value: Double
     private weak var coordinatorDelegate: QRcodeViewModelCoordinatorDelegate?
 
-    init(roomName: String, value: Double, coordinator: QRcodeViewModelCoordinatorDelegate, authService: AuthService) {
-        self.roomName = roomName
+    init(roomName: String, value: Double, coordinator: QRcodeViewModelCoordinatorDelegate, authService: AuthService, roomService: RoomService) {
         self.value = value
         self.coordinatorDelegate = coordinator
-        super.init(authService: authService)
+        super.init(roomName: roomName, authService: authService, roomService: roomService)
     }
 
     func generateStringForQRcode(completion: @escaping (String) -> Void) {
         guard let uid = userID else { return }
-        let string = "com.glrmeslp.EasyBank00X00\(roomName)00X00\(uid)00X00\(value)"
-        completion(string)
+        let value = value
+        let roomName = roomName
+        getUserName { userName in
+            let string = "com.glrmeslp.EasyBank00X00\(roomName)00X00\(value)00X00\(uid)00X00\(userName)"
+            completion(string)
+        }
     }
-    
+
     func getValue(completion: @escaping (String) -> Void) {
         if value == 0.0 {
             completion("Will be defined by who will pay")
