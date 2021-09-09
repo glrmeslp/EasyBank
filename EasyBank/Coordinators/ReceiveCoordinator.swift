e57ad8314992e262e7683e5fe87a1359910fb433
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-09 14:20:34 -0400

    update receive coordiantor

--- EasyBank/Coordinators/ReceiveCoordinator.swift
+++ EasyBank/Coordinators/ReceiveCoordinator.swift
@@ -2,15 +2,21 @@ import UIKit
 import FirebaseFirestore
 import FirebaseAuth
 
+protocol ReceiveViewCoordinatorDelegate: AnyObject {
+    func pushToQRCodeViewController(value: Double)
+}
+
 final class ReceiveCoordinator: Coordinator {
     var navigationController: UINavigationController
     private let roomName: String
     private let auth: Auth
+    private let firestore: Firestore
     
-    init(navigationController: UINavigationController, roomName: String, auth: Auth) {
+    init(navigationController: UINavigationController, roomName: String, auth: Auth, firestore: Firestore) {
         self.navigationController = navigationController
         self.roomName = roomName
         self.auth = auth
+        self.firestore = firestore
     }
 
     func start() {
@@ -22,7 +28,8 @@ final class ReceiveCoordinator: Coordinator {
 extension ReceiveCoordinator: ReceiveViewCoordinatorDelegate {
     func pushToQRCodeViewController(value: Double) {
         let authService = AuthenticationService(auth: auth)
-        let qrCodeViewModel = QRCodeViewModel(roomName: roomName, value: value, coordinator: self, authService: authService)
+        let roomService = DatabaseService(firestore: firestore)
+        let qrCodeViewModel = QRCodeViewModel(roomName: roomName, value: value, coordinator: self, authService: authService, roomService: roomService)
         let qrCodeViewController = QRCodeViewController(viewModel: qrCodeViewModel)
         navigationController.pushViewController(qrCodeViewController, animated: true)
     }
