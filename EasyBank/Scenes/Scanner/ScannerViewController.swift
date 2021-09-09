eebf3a7457f9ba51321492a32e0fbd515adfddf9
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-09 14:40:33 -0400

    update scanner view controller

--- EasyBank/Scenes/Scanner/ScannerViewController.swift
+++ EasyBank/Scenes/Scanner/ScannerViewController.swift
@@ -28,11 +28,6 @@ final class ScannerViewController: UIViewController {
         isCaptureSessionRunning()
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        isCaptureSessionRunning()
-    }
-
     private func isCaptureSessionRunning() {
         if (captureSession?.isRunning == true) {
             captureSession.stopRunning()
@@ -85,7 +80,6 @@ final class ScannerViewController: UIViewController {
     }
 
     private func found(code: String) {
-        captureSession.stopRunning()
         viewModel?.validateQRCode(with: code, from: self) { [weak self] isValid in
             if !isValid {
                 self?.presentAlert(with: "Oops! Something went wrong",
@@ -105,11 +99,10 @@ final class ScannerViewController: UIViewController {
 
 extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-
+        captureSession.stopRunning()
         if let metadataObject = metadataObjects.first {
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
             guard let stringValue = readableObject.stringValue else { return }
-            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
             found(code: stringValue)
         }
     }
