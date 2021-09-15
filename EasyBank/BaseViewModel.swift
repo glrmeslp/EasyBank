72598d2bb27be9ee08905a79d94beaf7cd915b3c
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-15 09:14:31 -0400

    update base view model

--- EasyBank/BaseViewModel.swift
+++ EasyBank/BaseViewModel.swift
@@ -4,22 +4,22 @@ class BaseViewModel {
     internal var userID: String?
     internal let roomName: String
     internal var userName: String?
-    
+
     init(roomName: String, authService: AuthService, roomService: RoomService) {
         self.roomName = roomName
         self.authService = authService
         self.roomService = roomService
         getUserID()
     }
-    
+
     private func getUserID() {
         authService.getUser { [weak self] user in
             guard let user = user else { return }
             self?.userID = user.uid
         }
     }
-    
-    internal func getUserName(completion: @escaping (String) -> Void) {
+
+    func getUserName(completion: @escaping (String) -> Void) {
         guard let uid = userID else { return }
         roomService.getAccount(roomName: roomName, uid: uid) { account, _ in
             guard let account = account else { return }
