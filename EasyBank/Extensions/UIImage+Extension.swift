d5c7cc454347394bcaf270e3687f16f4be9f8906
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-15 09:16:12 -0400

    update ui image extension

--- EasyBank/Extensions/UIImage+Extension.swift
+++ EasyBank/Extensions/UIImage+Extension.swift
@@ -3,16 +3,14 @@ import UIKit
 extension UIImage {
     func generateQRCode(from string: String) -> UIImage? {
         let data = string.data(using: String.Encoding.ascii)
-
             if let filter = CIFilter(name: "CIQRCodeGenerator") {
                 filter.setValue(data, forKey: "inputMessage")
-                let transform = CGAffineTransform(scaleX: 4, y: 4)
+                let transform = CGAffineTransform(scaleX: 5, y: 5)
 
                 if let output = filter.outputImage?.transformed(by: transform) {
                     return UIImage(ciImage: output)
                 }
             }
-
             return nil
     }
 }
