273cfe49ce9838145d01593f93a3375b3691a353
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-16 15:16:40 -0400

    create home menu screen

--- EasyBank/Scenes/HomeMenu/HomeMenuViewController.swift
+++ EasyBank/Scenes/HomeMenu/HomeMenuViewController.swift
@@ -1,29 +1,52 @@
-//
-//  HomeMenuViewController.swift
-//  EasyBank
-//
-//  Created by Guilherme de Sousa Peixoto on 15/09/21.
-//
-
 import UIKit
 
-class HomeMenuViewController: UIViewController {
+final class HomeMenuViewController: UIViewController {
+
+    private var viewModel: HomeMenuViewModel?
+
+    @IBOutlet private weak var passwordButton: UIButton!
+    @IBOutlet private weak var roomButton: UIButton!
+    @IBOutlet private weak var accountButton: UIButton!
+    @IBOutlet private weak var signOutButton: UIButton!
+
+    init(viewModel: HomeMenuViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: "HomeMenuView", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapPassordButton(_ sender: Any) {
+        viewModel?.updatePassword()
     }
-    */
 
+    @IBAction func didTapRoomButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Do you want to leave the room?", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Leave", style: .destructive) { _ in
+                            self.dismiss(animated: true)
+                            self.viewModel?.leaveRoom()
+        })
+        present(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func didTapAccountButton(_ sender: Any) {
+        viewModel?.showAccount()
+    }
+
+    @IBAction func didTapSignOutButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Sign Out", message: "Do you really want to leave?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Sign Out", style: .destructive) { _ in
+                            self.dismiss(animated: true)
+                            self.viewModel?.signOut()
+        })
+        present(alert, animated: true, completion: nil)
+    }
 }
--- EasyBank/Scenes/HomeMenu/HomeMenuViewModel.swift
+++ EasyBank/Scenes/HomeMenu/HomeMenuViewModel.swift
@@ -1,8 +1,26 @@
-//
-//  HomeMenuViewModel.swift
-//  EasyBank
-//
-//  Created by Guilherme de Sousa Peixoto on 15/09/21.
-//
+final class HomeMenuViewModel {
+    private let authService: AuthService
+    private var coordinatorDelegate: HomeMenuViewModelCoordinatorDelegate?
 
-import Foundation
+    init(authService: AuthService, coordinator: HomeMenuViewModelCoordinatorDelegate) {
+        self.authService = authService
+        self.coordinatorDelegate = coordinator
+    }
+
+    func updatePassword() {
+        print("Password Button Tapped")
+    }
+
+    func leaveRoom() {
+        coordinatorDelegate?.pushToStartViewController()
+    }
+
+    func showAccount() {
+        print("Account Button Tapped")
+    }
+
+    func signOut() {
+        authService.signOut()
+        coordinatorDelegate?.pushToStartViewController()
+    }
+}
