0cf0404d49cf62908d76c8a1693d1d5b095f7dc1
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-16 11:26:47 -0400

    create home menu and add icons

---
+++ EasyBank/Scenes/HomeMenu/HomeMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeMenuViewController.swift
+//  EasyBank
+//
+//  Created by Guilherme de Sousa Peixoto on 15/09/21.
+//
+
+import UIKit
+
+class HomeMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EasyBank/Scenes/HomeMenu/HomeMenuViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeMenuViewModel.swift
+//  EasyBank
+//
+//  Created by Guilherme de Sousa Peixoto on 15/09/21.
+//
+
+import Foundation
