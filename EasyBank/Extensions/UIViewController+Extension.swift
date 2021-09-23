d09a6c1e110a7677ab4af28672758ce2655dbd14
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-23 09:49:27 -0400

    create present action sheet func

--- EasyBank/Extensions/UIViewController+Extension.swift
+++ EasyBank/Extensions/UIViewController+Extension.swift
@@ -7,6 +7,13 @@ extension UIViewController {
         alert.addAction(UIAlertAction(title: buttonTitle, style: .cancel, handler: handler))
         present(alert, animated: true, completion: nil)
     }
+
+    func presentActionSheet(title: String, buttonTitle: String, message: String? = nil, style: UIAlertAction.Style = .default, handler: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: buttonTitle, style: style, handler: handler))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(alert, animated: true)
+    }
     
     func setupNavigationController(isHidden: Bool) {
         let backBarButtonItem =  UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
