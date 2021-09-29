bb18830ad97144b056f2e4486aa730e562bd924b
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-09-29 09:22:08 -0400

    add bank button

--- EasyBank/Scenes/HomeMenu/HomeMenuViewController.swift
+++ EasyBank/Scenes/HomeMenu/HomeMenuViewController.swift
@@ -42,6 +42,15 @@ final class HomeMenuViewController: UIViewController {
         viewModel?.showAccount()
     }
 
+    @IBAction func didTapBankButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Enter bank mode", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Ok", style: .default) { _ in
+                            self.dismiss(animated: true)
+        })
+        present(alert, animated: true, completion: nil)
+    }
+
     @IBAction private func didTapSignOutButton(_ sender: Any) {
         let alert = UIAlertController(title: "Sign Out", message: "Do you really want to leave?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
