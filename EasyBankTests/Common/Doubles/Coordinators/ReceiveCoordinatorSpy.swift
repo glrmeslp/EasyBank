f4066d256d86f28d493f6149ecf24ebf275858fc
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-10-22 11:03:28 -0400

    Update qr code view model tests

--- EasyBankTests/Common/Doubles/Coordinators/ReceiveCoordinatorSpy.swift
+++ EasyBankTests/Common/Doubles/Coordinators/ReceiveCoordinatorSpy.swift
@@ -2,6 +2,7 @@
 
 final class ReceiveCoordinatorSpy {
     private(set) var pushToQRCodeViewControllerCalled = false
+    private(set) var didFinishCalled = false
     private(set) var valueReturned = 0.0
 }
 
@@ -9,5 +10,11 @@ extension ReceiveCoordinatorSpy: ReceiveViewCoordinatorDelegate {
     func pushToQRCodeViewController(value: Double) {
         pushToQRCodeViewControllerCalled = true
         valueReturned = value
+    }
+}
+
+extension ReceiveCoordinatorSpy: QRcodeViewModelCoordinatorDelegate {
+    func didFinisih() {
+        didFinishCalled = true
     }
 }
--- EasyBankTests/Scenes/QRCode/QRCodeViewModelTests.swift
+++ EasyBankTests/Scenes/QRCode/QRCodeViewModelTests.swift
@@ -1,4 +1,37 @@
 import XCTest
+@testable import EasyBank
 
 final class QRCodeViewModelTests: XCTestCase {
+    private let authServiceSpy = AuthenticationServiceSpy()
+    private let coordinatorSpy = ReceiveCoordinatorSpy()
+    private lazy var sut: QRCodeViewModel = QRCodeViewModel(roomName: "Room",
+                                                            value: 0.0,
+                                                            coordinator: coordinatorSpy,
+                                                            authService: authServiceSpy)
+
+    func test_didFinish_shouldCallCoordinatorDidFinish() {
+        sut.didFinish()
+        XCTAssertTrue(coordinatorSpy.didFinishCalled)
+    }
+
+    func test_generateStringForQRcode_shouldReturnString() {
+        authServiceSpy.user = User(identifier: "123", name: "Guilherme", email: "test@test.com")
+        sut.generateStringForQRcode { result in
+            XCTAssertEqual("com.glrmeslp.EasyBank00X00Room00X000.000X0012300X00Guilherme", result)
+        }
+    }
+
+    func test_getValue_givenValueZero_shouldReturnString() {
+        sut.getValue { result in
+            XCTAssertEqual("Will be defined by who will pay", result)
+        }
+    }
+
+    func test_getValue_givenValueTen_shouldReturnValueAsCurrency() {
+        let value = 10.0
+        sut = QRCodeViewModel(roomName: "Room", value: value, coordinator: coordinatorSpy, authService: authServiceSpy)
+        sut.getValue { result in
+            XCTAssertEqual(value.asCurrency(), result)
+        }
+    }
 }
