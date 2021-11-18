26a73a67f0d6fae0c31e64f7a6f7ea6affb5904c
Guilherme de Sousa Peixoto <gspeixoto@outlook.com>
2021-11-18 10:36:18 -0400

    tests: create example test with quick nimble

---
+++ EasyBankTests/ViewModelTests.swift
@@ -0,0 +1,31 @@
+
+import Quick
+import Nimble
+@testable import EasyBank
+
+final class ViewModelTests: QuickSpec {
+    override func spec() {
+        var sut: BankViewModel!
+        var roomServiceSpy: DatabaseServiceSpy!
+        
+        beforeEach {
+            UserDefaults.standard.set("Room01", forKey: "Room")
+            roomServiceSpy = DatabaseServiceSpy()
+            sut = BankViewModel(roomService: roomServiceSpy)
+        }
+        
+        describe("#fetchRoomName") {
+            var result: String!
+            
+            beforeEach {
+                sut.fetchRoomName { name in
+                    result = name
+                }
+            }
+            
+            it("deveria retornar nome da sala correto") {
+                expect(result) == "Room01"
+            }
+        }
+    }
+}
