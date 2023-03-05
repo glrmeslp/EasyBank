bd4702918378adb65b8ee63347946b9a7a81ea7e
Guilherme de Sousa Peixoto <guilherme.peixoto@NMAC-4725.local>
2023-03-04 21:47:19 -0400

    Deleted BankViewOld

--- EasyBank/Scenes/Bank/BankViewController.swift
+++ EasyBank/Scenes/Bank/BankViewController.swift
@@ -35,43 +35,6 @@ final class BankViewController: ViewController<BankInteractor, UIView> {
     override func configureViews() {
         view.backgroundColor = .systemBackground
     }
-    //
-//    private var viewModel: BankViewModelDelegate?
-//    private var customView: BankViewDelegate {
-//        return view as! BankViewDelegate
-//    }
-//
-//    init(viewModel: BankViewModelDelegate) {
-//        self.viewModel = viewModel
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//    }
-//
-//    override func loadView() {
-//        view = BankView()
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        fetchData()
-//    }
-//
-//    private func fetchData() {
-//        viewModel?.fetchBankMenu { [weak self] menu in
-//            self?.customView.setup(menu)
-//        }
-//
-//        viewModel?.fetchRoomName { [weak self] roomName in
-//            self?.customView.setup(roomName)
-//        }
-//
-//        viewModel?.fetchAccounts { [weak self] accounts in
-//            self?.customView.setup(accounts)
-//        }
-//    }
 }
 
 extension BankViewController: BankDisplaying { }
